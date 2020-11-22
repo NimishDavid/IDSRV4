a325f5d523edbb9e607ee0b0d15d0fd5d6dee67e
Nimish David Mathew <nimish.mathew@experionglobal.com>
2020-11-22 20:42:28 +0530

    Identity server added.

---
+++ Extensions/RSAExtensions.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Xml;
+
+namespace AspNetIdentity_Server.Extensions
+{
+    public static class RSAExtensions
+    {
+        /// <summary>
+        /// Gets RSA Parameters from XML file.
+        /// </summary>
+        /// <param name="xmlFilePath">The XML file path.</param>
+        /// <returns>RSAParameters.</returns>
+        /// <exception cref="Exception">Invalid XML RSA key.</exception>
+        public static RSAParameters RSAParametersFromXmlFile(string xmlFilePath)
+        {
+            RSAParameters parameters = new RSAParameters();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(File.ReadAllText(xmlFilePath));
+
+            if (xmlDoc.DocumentElement.Name.Equals("RSAKeyValue"))
+            {
+                foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes)
+                {
+                    switch (node.Name)
+                    {
+                        case "Modulus": parameters.Modulus = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "Exponent": parameters.Exponent = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "P": parameters.P = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "Q": parameters.Q = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "DP": parameters.DP = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "DQ": parameters.DQ = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "InverseQ": parameters.InverseQ = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "D": parameters.D = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid XML RSA key.");
+            }
+
+            return parameters;
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using AspNetIdentity_Server.Data;
 using AspNetIdentity_Server.Data.Models;
+using AspNetIdentity_Server.Extensions;
+using IdentityServer4.WsFederation.Configuration;
+using IdentityServer4.WsFederation.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +16,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace AspNetIdentity_Server
 {
@@ -20,6 +25,7 @@ namespace AspNetIdentity_Server
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            Config.Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -48,6 +54,29 @@ namespace AspNetIdentity_Server
                 .AddDefaultUI()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
+
+            var builder = services.AddIdentityServer(options =>
+        {
+            options.Events.RaiseErrorEvents = true;
+            options.Events.RaiseInformationEvents = true;
+            options.Events.RaiseFailureEvents = true;
+            options.Events.RaiseSuccessEvents = true;
+
+            // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
+            options.EmitStaticAudienceClaim = true;
+        })
+            .AddInMemoryIdentityResources(Config.IdentityResources)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddInMemoryClients(Config.Clients)
+            .AddAspNetIdentity<User>()
+            .AddSigningCredential(InitializeRsaKey())
+            .AddWsFederationPlugin(options => new WsFederationOptions
+            {
+                // WsFederationEndpoint = "https://localhost:5001/wsfed",
+                Licensee = "DEMO",
+                LicenseKey = "eyJTb2xkRm9yIjowLjAsIktleVByZXNldCI6NiwiU2F2ZUtleSI6ZmFsc2UsIkxlZ2FjeUtleSI6ZmFsc2UsIlJlbmV3YWxTZW50VGltZSI6IjAwMDEtMDEtMDFUMDA6MDA6MDAiLCJhdXRoIjoiREVNTyIsImV4cCI6IjIwMjAtMTItMTlUMDA6MDA6MDAiLCJpYXQiOiIyMDIwLTExLTE5VDEwOjE4OjI2Iiwib3JnIjoiREVNTyIsImF1ZCI6M30=.UhTAxychYJHyF39zhgwN8waQn3nraIW2Qm2Sw+wb5ISnqzO3KJbr+t/mp8w6NdgMlzj7SXZ36QqkP5/c9hdaIzS+/2UPl6ndSI7B4+up4FlhAntEDBumODY6Pgr5IF/BcFEvk09vm65cA84DRMsDcMeREDkkjYNGVbeJjLzpOzJ6QmQifVplYIblR38A9qYDeTdToh9dI8zT1n8McZWW8tYi9YES8/YJM4UBxBkuO8Ej6haFihPeVZkXmzAOkQRWRwy4+06VxC4tGzJeVWNK4vBLEpZxLTr81To2MnRhhNjzjqx/oaJfm7FmcqhVlcLobFTsoDy8pQmIK9tO5mF+aoic6TJBvahPIwt7BIsJHFMjqM4RNw/97T81WMhIAdev9rrhVTVLonyxgko24JCOUQIGnvaKF4eFZ4bKcsDNr1ymOjgdM5zFGwuLAAY+Okk+Ftl5TPa/x5aZSIg6SI6dZPD9dxFuC5e3kLCniSS6fuz2K+cKqdOBtq2lJyloWZt1yyoC9+suy45NrLOMVQeWwsXqysACFjPVU3OBoCFD00Vt3vaw6x8sv7bdRZdM8PRfRdB+A09bOhmq3PUA8pTTcDTuyRS5cncyGMlTkwnFybbziAWGDVQP0hug4HJdfRF6c2BmqP6/S1Js7ULPLSevyKQzs7BJmYcKlHmSN04QlrE="
+            })
+            .AddInMemoryRelyingParties(new List<RelyingParty>());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,13 +98,30 @@ namespace AspNetIdentity_Server
 
             app.UseRouting();
 
-            app.UseAuthentication();
+            app.UseIdentityServer().UseIdentityServerWsFederationPlugin();
+            // app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
             });
+        }
+
+        private SigningCredentials InitializeRsaKey()
+        {
+            try
+            {
+                RSACryptoServiceProvider rsaProvider = new RSACryptoServiceProvider(2048);
+                var rsaParametersPrivate = RSAExtensions.RSAParametersFromXmlFile("private-rsa-key.xml");
+                rsaProvider.ImportParameters(rsaParametersPrivate);
+                var securityKey = new RsaSecurityKey(rsaProvider);
+                return new SigningCredentials(securityKey, SecurityAlgorithms.RsaSha256);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Identity Server RSA Key initialization failed. " + ex.ToString());
+            }
         }
     }
 }
